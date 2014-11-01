d6ec907bc4e1b781e843c97b76dd45e1b0382b3e
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-11-01 10:10:26 +0530

    Fix: bugs

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 xEnquiryNSubscription
 =====================
 
-<<<<<<< HEAD
 xEpan Application for Subscriptions and NewsLetters. Application is also having Cutom Enquiry form with data stoe as well as email options.
-=======
-Enquiry and Subscription tools with Newsletter. A lot of activities in xEpan CMS are Based on this component
->>>>>>> b2eaa1a10532371f69e1fd7743769ab093f99c95
--- lib/Model/EmailJobs.php
+++ lib/Model/EmailJobs.php
@@ -14,10 +14,13 @@ class Model_EmailJobs extends \Model_Table {
 		// $this->hasOne('xEnquiryNSubscription/Subscription','subscriber_id');
 		// $this->addField('email');
 		$this->addField('job_posted_at')->type('datetime')->defaultValue(date('Y-m-d H:i:s'));
-		$this->addField('processed')->type('boolean')->defaultValue(false);
 		$this->addField('processed_on')->type('datetime')->defaultValue(null);
 
 		// $this->addField('post_socials_and_blogs')->type('boolean')->defaultValue(false);
+
+		// $this->addExpression('processed')->set(function($m,$q){
+		// 	return $m->refSQL('xEnquiryNSubscription/EmailQueue')->addCondition('is_sent',false)->count();
+		// })->type('boolean');
 
 		$this->addExpression('processed_in_hour')->set('DATE_FORMAT(processed_on,"%Y-%m-%d %H:00:00")');
 
