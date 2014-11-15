dbc79cae6d7a0f1c3fda92b19ead70acc938f3e4
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-11-15 19:50:57 +0530

    short_description not for social now

--- lib/Model/NewsLetter.php
+++ lib/Model/NewsLetter.php
@@ -13,7 +13,7 @@ class Model_NewsLetter extends \Model_Table {
 		$this->addCondition('epan_id',$this->api->current_website->id);
 
 		$this->addField('name');
-		$this->addField('short_description')->hint('255 Characters Msg for social and tweets');
+		$this->addField('short_description');//->hint('255 Characters Msg for social and tweets');
 		$this->addField('email_subject');
 		$this->addField('matter')->type('text')->display(array('form'=>'RichText'))->defaultValue('<p></p>');
 		// $this->add('dynamic_model/Controller_AutoCreator');
