780e8454c51e95e8f5be0cf417a79c849e1cb4ca
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-11-20 11:05:01 +0530

    Matter made mandatory in emails

--- lib/Model/NewsLetter.php
+++ lib/Model/NewsLetter.php
@@ -16,7 +16,16 @@ class Model_NewsLetter extends \Model_Table {
 		$f->icon='fa fa-adn~red';
 		// $this->addField('short_description')->display(array('grid'=>'shorttext,wrap'));//->hint('255 Characters Msg for social and tweets');
 		$this->addField('email_subject')->mandatory(true)->group('a~12~<i/> NewsLetter');
-		$this->addField('matter')->type('text')->display(array('form'=>'RichText'))->defaultValue('<p></p>')->group('a~12~bl');
+		$this->addField('matter')->type('text')->display(array('form'=>'RichText'))->defaultValue('<p></p>')->group('a~12~bl')->mandatory(true);
+
+		$this->addHook('beforeSave',$this);
+
 		// $this->add('dynamic_model/Controller_AutoCreator');
 	}
+
+	function beforeSave(){
+		if($this['matter']=='<p></p>')
+			throw $this->exception('Matter is mandatory field','ValidityCheck')->setField('matter');
+	}
+
 }
